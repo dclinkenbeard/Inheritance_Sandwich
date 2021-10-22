d38036684903878925fa5fc63856bc5f74868f14
Dr.C <dclinkenbeard@csumb.edu>
2021-10-22 14:11:13 -0700

    Initial commit

---
+++ src/inClass/Deli.java
@@ -0,0 +1,24 @@
+package inClass;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Deli {
+    public static void main(String[] args){
+        HashMap<String,Integer> filling = new HashMap<>();
+        filling.put("pepper jack", 2);
+        filling.put("gouda",2);
+        Sandwich grilledCheese = new DeliSandwich(2,filling);
+        Burger doubleDouble = new Burger();
+//        System.out.println(grilledCheese);
+
+        ArrayList<Sandwich> menu = new ArrayList<>();
+        menu.add(grilledCheese);
+        menu.add(doubleDouble);
+
+        for(Sandwich s : menu ){
+            System.out.println(s.toString());
+        }
+
+    }
+}
---
+++ src/inClass/Sandwich.java
@@ -0,0 +1,7 @@
+package inClass;
+
+import java.util.HashMap;
+
+public interface Sandwich {
+      public String listFilling();
+}
